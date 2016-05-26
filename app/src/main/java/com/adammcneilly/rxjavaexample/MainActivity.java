f761153fa859486e1c74ce105336ec782264df8a
AdamMc331 <amcneilly331@gmail.com>
2016-05-26 07:44:30 -0400

    Firt commit

---
+++ app/src/main/java/com/adammcneilly/rxjavaexample/MainActivity.java
@@ -0,0 +1,117 @@
+package com.adammcneilly.rxjavaexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createHelloWorldObservable();
+        createHelloWorldObservableSimpler();
+        createHelloWorldObservableChained();
+        createHelloWorldObservableWithMaps();
+    }
+
+    private void createHelloWorldObservable() {
+        // Create observable
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello, world!");
+                subscriber.onCompleted();
+            }
+        });
+
+        // Create subscriber
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.v(LOG_TAG, s);
+            }
+        };
+
+        // Subscribe to observable
+        observable.subscribe(subscriber);
+    }
+
+    private void createHelloWorldObservableSimpler() {
+        // Simplify using Observable.just() which emits a single item then completes:
+        Observable<String> observable = Observable.just("Hello, world!");
+
+        // Since we don't care about onCompleted() or onError(), use a simpler class to define what to do in onNext().
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.v(LOG_TAG, s);
+            }
+        };
+
+        // Observable.subscribe() can handle one, two, or three Action parameter for onNExt, onError, and onComplete
+        // Since we just have onNext, ew only need the first one.
+        observable.subscribe(onNextAction);
+    }
+
+    private void createHelloWorldObservableChained() {
+        // Get rid of variables by chaining
+        Observable.just("Hello, world!").subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.v(LOG_TAG, s);
+            }
+        });
+    }
+
+    private void createHelloWorldObservableWithMaps() {
+        // Use map operator to transform one emitted item into another
+        Observable.just("Hello, world!")
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        return s + " - Adam";
+                    }
+                })
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.v(LOG_TAG, s);
+                    }
+                });
+
+        // Note that type doesn't ahve to be the same
+        // Use map to send hash code for string instead
+        Observable.just("Hello, world!")
+                .map(new Func1<String, Integer>() {
+                    @Override
+                    public Integer call(String s) {
+                        return s.hashCode();
+                    }
+                })
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.v(LOG_TAG, String.valueOf(integer));
+                    }
+                });
+    }
+}
